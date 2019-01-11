f928bea85ba8d182b2defa0af725e26ea2370ea8
lubitelpospat <sizykh.ad@phystech.edu>
2019-01-11 12:14:33 +0300

    Added feature of requesting dog photos. First commit.

--- main.py
+++ main.py
@@ -2,21 +2,28 @@ from telegram.ext import Updater, CommandHandler
 import requests
 
 
-def get_url():
-     r=requests.get('http://aws.random.cat/meow').json()
+def get_cat_url():
+     r = requests.get('http://aws.random.cat/meow').json()
      return r['file']
 
+def get_dog_url():
+     r = requests.get('https://dog.ceo/api/breeds/image/random').json()
+     return r['message']
 
-def drop(bot, update):
-    url = get_url()
+def drop_cat(bot, update):
+    url = get_cat_url()
     chat_id = update.message.chat_id
     bot.send_photo(chat_id=chat_id, photo=url)
 
-
+def drop_dog(bot, update):
+    url = get_dog_url()
+    chat_id = update.message.chat_id
+    bot.send_photo(chat_id=chat_id, photo=url)
 def main():
     updater = Updater('782286279:AAG2hy2CZ8SxPBhP8bccuxZhF9h6sBZ3H_E')
     dp = updater.dispatcher
-    dp.add_handler(CommandHandler('cat', drop))
+    dp.add_handler(CommandHandler('cat', drop_cat))
+    dp.add_handler(CommandHandler('dog', drop_dog))
     updater.start_polling()
     updater.idle()
 
